c519561a928dcf230d530dc6e24276e31c208bb8
akram ahil <akram.ahil123@gmail.com>
2024-08-10 15:53:24 +0100

    contact

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/navbar/navbar'
+import Contact from '../components/contact/contact'
+import Footer from '../components/footer/footer'
+
+export default function contact() {
+  return (
+    <div>
+      <Navbar/>
+      <Contact/>
+      <Footer/>
+    </div>
+  )
+}
