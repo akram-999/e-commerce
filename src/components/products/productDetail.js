98bcb4ecee4a44f886c293b6584fda3b2daf83c6
akram ahil <akram.ahil123@gmail.com>
2024-08-16 09:21:54 +0100

    upd

--- src/components/products/productDetail.js
+++ src/components/products/productDetail.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Products} from './data'
-import { useParams } from "react-router-dom"
+import {Link, useParams } from "react-router-dom"
 
 export default function ProductDetail() {
     const {id} = useParams();
@@ -104,44 +104,30 @@ export default function ProductDetail() {
               >
                 (5.0)
               </p>
-              <a
-                href="#"
+              <p
+                
                 class="text-sm font-medium leading-none text-gray-900 underline hover:no-underline dark:text-white"
               >
                 345 Reviews
-              </a>
+              </p>
             </div>
           </div>
 
           <div class="mt-6 sm:gap-4 sm:items-center sm:flex sm:mt-8">
             
 
-            <a
-              href="#"
-              title=""
+            <Link
+              to="/cart"
+              
               class="text-white mt-4 sm:mt-0 bg-violet-700 hover:bg-violet-800 decoration-transparent focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center justify-center"
               role="button"
             >
-              <svg
-                class="w-5 h-5 -ms-2 me-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6"
-                />
-              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6  text-white mr-2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                </svg>
 
               Add to cart
-            </a>
+            </Link>
           </div>
 
           <hr class=" border-gray-200 dark:border-gray-800" />
